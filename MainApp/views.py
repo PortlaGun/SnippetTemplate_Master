2a170196fd5b03f71811e5f7eb5810e9fce7b2f9
PortlaGun <killhiggers@gmail.com>
2023-07-17 12:42:11 +0300

    DB + snippet page

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,8 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
-
+from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -15,3 +17,26 @@ def add_snippet_page(request):
 def snippets_page(request):
     context = {'pagename': 'Просмотр сниппетов'}
     return render(request, 'pages/view_snippets.html', context)
+
+
+def create_snippet(request):
+   form = SnippetForm()
+   return render(request, 'add_snippet.html', {'form': form})
+
+
+# получение данных из бд
+def get_snippets(request):
+    snippets = Snippet.objects.all()
+    for snippet in snippets:
+        snippet.creation_date = snippet.creation_date.strftime("%Y-%m-%d %H:%M")
+    context = {"snippets": snippets, "hide": False}
+    return render(request, "pages/view_snippets.html", context)
+
+def get_snippet(request):
+    try:
+        snippet = Snippet.objects.get(id=request.GET.get("snippet_id"))
+        snippet.creation_date = snippet.creation_date.strftime("%Y-%m-%d %H:%M")
+        context = {'snippet': snippet}
+        return render(request, "pages/view_snippet.html", context)
+    except Snippet.DoesNotExist:
+        return HttpResponseNotFound("<h2>snippet not found</h2>")
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -8,5 +8,6 @@ from MainApp import views
 urlpatterns = [
     path('', views.index_page),
     path('snippets/add', views.add_snippet_page, name='add_snippet_page'),
-    path('snippets/list', views.snippets_page, name='view_snippets_page'),
+    path('snippets/list', views.get_snippets, name='view_snippets_page'),
+    path('snippets/', views.get_snippet, name='view_snippet_page'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
